f17be11f10f93a158d6eea198638ebe3441fe099
kibriatuhin <tuinn78@gmail.com>
2022-09-06 07:23:37 +0600

    project push in github

---
+++ src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.home_practice_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePracticeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomePracticeSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/home_practice_springboot/HomePracticeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_practice_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePracticeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
