e90340d352ebcde40195edb16f63bc3baac7278b
kibriatuhin <tuinn78@gmail.com>
2022-09-12 20:03:15 +0600

    ioc container & bean basic practice

--- src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
+++ src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
@@ -1,13 +1,30 @@
 package com.example.home_practice_springboot;
 
+import com.example.home_practice_springboot.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class HomePracticeSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HomePracticeSpringBootApplication.class, args);
+        //SpringApplication.run(HomePracticeSpringBootApplication.class, args);
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println(context.getBeanDefinitionCount());
+
+        Student student = context.getBean("student1", Student.class);
+        System.out.println(student);
+        //Student student1 =   context.getBean("student1",Student.class);
+
+        //System.out.println(student1);
+
+        // ((ClassPathXmlApplicationContext)context).registerShutdownHook();
+
+
+
+
     }
 
 }
