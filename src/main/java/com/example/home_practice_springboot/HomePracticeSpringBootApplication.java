6a7442f14285236c47a85e558f8505ceafb0b214
kibriatuhin <tuinn78@gmail.com>
2022-09-21 08:04:42 +0600

    customize scope and lifecycle callback

--- src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
+++ src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
@@ -2,7 +2,9 @@ package com.example.home_practice_springboot;
 
 import com.example.home_practice_springboot.model.Student;
 import com.example.home_practice_springboot.model.Student2;
+import com.example.home_practice_springboot.model.ThreadScopeTestBean;
 import com.example.home_practice_springboot.service.CommandExecutorService;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,8 +16,20 @@ public class HomePracticeSpringBootApplication {
     public static void main(String[] args) {
 
     ApplicationContext context = new ClassPathXmlApplicationContext("beans2.xml");
-    CommandExecutorService service = context.getBean(CommandExecutorService.class);
-    service.processCommand();
+
+        ThreadScopeTestBean testBean = context.getBean("threadScopeTestBean",ThreadScopeTestBean.class);
+
+        System.out.println("object 1 : " + Thread.currentThread().getName() + " :: "+ testBean);
+
+        new Thread(()-> {
+                ThreadScopeTestBean testBean1 =context.getBean("threadScopeTestBean",ThreadScopeTestBean.class);
+
+                System.out.println("Object 2 :: " +Thread.currentThread().getName()+ " :: " + testBean1);
+
+        }).start();
+
+
+
     }
 
 }
