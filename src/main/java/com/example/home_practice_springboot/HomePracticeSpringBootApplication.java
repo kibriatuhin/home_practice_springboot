742fa25a51c1f90a67cc5bb37d72bede292cd5e6
kibriatuhin <tuinn78@gmail.com>
2022-09-24 11:08:26 +0600

    customize scope and lifecycle callback

--- src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
+++ src/main/java/com/example/home_practice_springboot/HomePracticeSpringBootApplication.java
@@ -1,44 +1,53 @@
 package com.example.home_practice_springboot;
 
+import com.example.home_practice_springboot.model.Simple;
 import com.example.home_practice_springboot.model.Student;
 import com.example.home_practice_springboot.model.Student2;
 import com.example.home_practice_springboot.model.ThreadScopeTestBean;
 import com.example.home_practice_springboot.service.CommandExecutorService;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HomePracticeSpringBootApplication {
 
     public static void main(String[] args) {
 
-    ApplicationContext context = new ClassPathXmlApplicationContext("beans2.xml");
-
-        ThreadScopeTestBean testBean = context.getBean("threadScopeTestBean",ThreadScopeTestBean.class);
-
-        System.out.println("object 1 : " + Thread.currentThread().getName() + " :: "+ testBean);
-
-        new Thread(()-> {
-                ThreadScopeTestBean testBean1 =context.getBean("threadScopeTestBean",ThreadScopeTestBean.class);
-
-                System.out.println("Object 2 :: " +Thread.currentThread().getName()+ " :: " + testBean1);
-
-        }).start();
-
-
-
     }
 
 }
+
+/*
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("com.example.home_practice_springboot.model","com.example.home_practice_springboot.scope");
+
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        context.refresh();
+        Object ob = context.getBean("simple");
+        System.out.println(ob);*/
+
+
+/*ApplicationContext context = new ClassPathXmlApplicationContext("beans3.xml");
+        Simple simple = context.getBean(Simple.class);
+        System.out.println(simple);
+
+        //PropertyPlaceholderConfigurer
+       // PropertySourcesPlaceholderConfigurer*/
 /*ApplicationContext context = new ClassPathXmlApplicationContext("beans2.xml");
         Student2 student2 = context.getBean("student",Student2.class);
         System.out.println(student2.getAddress());
         Student2 student3 = context.getBean("student",Student2.class);
         System.out.println(student3.getAddress());*/
-      /*  //SpringApplication.run(HomePracticeSpringBootApplication.class, args);
+/*  //SpringApplication.run(HomePracticeSpringBootApplication.class, args);
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         System.out.println(context.getBeanDefinitionCount());
 
